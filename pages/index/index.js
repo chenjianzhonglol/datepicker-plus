4e495ff0562fbb320690fc171986df4c4693adc0
chenjz <842289456@qq.com>
2018-02-26 15:59:57 +0800

    datepicker-plus init

---
+++ pages/index/index.js
@@ -0,0 +1,23 @@
+Page({
+  data: {
+    show: true,
+    dateInfo: {},
+  },
+  submit: function(e) {
+    console.log(e);
+    console.log('日期类型：' + e.detail.dateType)
+    console.log('是否显示年份：' + e.detail.showYear)
+    console.log('日期字符串：' + e.detail.dateStr)
+    this.setData({
+      dateInfo: e.detail
+    })
+  },
+  showDatePickerPlus: function() {
+    this.setData({
+      show: true
+    })
+  },
+  onLoad: function () {
+    
+  },
+})
